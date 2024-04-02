2ec7a97e75a4334952e6506b26f77d15bcc2039a
Aren <197610445@qq.com>
2024-04-02 15:32:37 +0800

    build: 项目初始化

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  singleQuote: false, //使用单引号
+  printWidth: 80,
+  jsxBracketSameLine: false, // 设置为true时,将多行JSX元素的 > 放在最后一行的末尾，而不是单独放在下一行
+  bracketSpacing: true, //对象大括号直接是否有空格，默认为 true，效果：{ foo: bar }
+  // trailingComma: "all",
+  endOfLine: "lf",
+  proseWrap: "never",
+  arrowParens: "always",
+  htmlWhitespaceSensitivity: "ignore",
+  proseWrap: "preserve", //不知道怎么翻译
+};
