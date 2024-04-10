daf678a5c862fb109b8e5ecc4b07ba7adfdec144
Aren <197610445@qq.com>
2024-04-10 11:23:48 +0800

    build: 修改提交样式校验

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,14 @@
 export default {
-  "*.{js,ts,vue,jsx,tsx,less}": ["yarn prettier"],
-  "*.{js,ts,vue,jsx,tsx}": ["yarn lint:es"],
+  "*.{js,ts,vue,jsx,tsx,less}": (files) => {
+    // 格式化js、ts、vue、jsx、tsx、less文件
+    const str = (files || []).join(" ");
+    return `prettier --config .prettierrc.cjs --write ${str}`;
+  },
+  "*.{js,ts,vue,jsx,tsx}": (files) => {
+    // 格式化js、ts、vue、jsx、tsx文件
+    const str = (files || []).join(" ");
+    return `eslint --fix ${str}`;
+  },
   "*.{rs}": (files) => {
     // 格式化rust文件
     const str = (files || []).join(" ");
